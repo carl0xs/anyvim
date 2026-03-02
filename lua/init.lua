d667d83260c6227a035b723939edcb568c131e8b
Carlos Henrique <ch.henriquevinha@gmail.com>
2026-03-02 15:39:41 -0300

    add vim-tmux-navigator lua

--- lua/init.lua
+++ lua/init.lua
@@ -387,6 +387,24 @@ require("lazy").setup({
     opts = {
       -- files = { "*.md" }, -- any .md file (instead of defaults)
     },
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
   }
 })
 
